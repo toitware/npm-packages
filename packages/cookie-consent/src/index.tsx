26d34abaccf420a30d94990713a3ed67f6e6730b
qvisten999 <jqjqvist@gmail.com>
2021-04-15 12:16:54 +0200

    wip

--- packages/cookie-consent/src/index.tsx
+++ packages/cookie-consent/src/index.tsx
@@ -1,12 +1,15 @@
 import {
   Button,
   Card,
-  Link,
+  IconButton,
+  Link as LinkCore,
   makeStyles,
   Theme,
   Typography,
 } from "@material-ui/core";
+import Cookies from "js-cookie";
 import React, { useEffect, useState } from "react";
+import { FiX } from "react-icons/fi";
 
 const useStyles = makeStyles((theme: Theme) => ({
   button: {
@@ -32,42 +35,131 @@ const useStyles = makeStyles((theme: Theme) => ({
     left: theme.spacing(2),
     zIndex: 10020,
     width: "calc(100% - 32px)",
+    overflow: "visible",
+  },
+  exitButton: {
+    position: "absolute",
+    top: -10,
+    right: -10,
   },
 }));
 
+let loadedAnalytics = false;
+
 interface CookieProps {
   segmentKey: string;
+  show: boolean;
+  callback: (show: boolean) => void;
 }
 
-function Cookie({ segmentKey }: CookieProps): JSX.Element {
-  const [manageCookies, setManageCookies] = useState<boolean>(false);
-  const [showCookieConsent, setShowCookiesConsent] = useState<boolean>(true);
+function Cookie({ segmentKey, show, callback }: CookieProps): JSX.Element {
   const classes = useStyles();
+  const [isUserConsent, setUserConsent] = useState<boolean | null>(null);
+  const [isPageLoaded, setPageLoaded] = useState<boolean>(false);
+  const [isUserSignedIn, setUserSignedIn] = useState<boolean>(false);
+  const [manageCookies, setManageCookies] = useState<boolean>(false);
+  const [showCookieConsent, setShowCookiesConsent] = useState<boolean>(
+    show
+      ? true
+      : Cookies.get("toit-cookies") === "true" ||
+        (typeof window !== "undefined" &&
+          window.sessionStorage?.getItem("disallow-cookies") === "true")
+      ? false
+      : true
+  );
+
+  const handleAcceptCookieUI = () => {
+    Cookies.set("toit-cookies", "true", {
+      path: "/",
+      expires: new Date(new Date().setFullYear(new Date().getFullYear() + 1)),
+    });
+    setUserConsent(true);
+    setShowCookiesConsent(false);
+    window.sessionStorage.removeItem &&
+      window?.sessionStorage?.removeItem("disallow-cookies");
+    callback(false);
+    window.location.reload();
+  };
+
+  const handleAcceptCookie = () => {
+    if (analytics && !loadedAnalytics) {
+      // Setup segment
+      loadedAnalytics = true;
+      let segmentAPIKey = segmentKey;
+
+      // Check if the meta segment-key is set.
+      const segmentKeyDOM = document.querySelector('meta[name="segment-key"]');
+      if (segmentKeyDOM) {
+        segmentAPIKey = segmentKeyDOM.getAttribute("content") || segmentAPIKey;
+      }
+
+      if (analytics.load && segmentAPIKey) {
+        analytics.load(segmentAPIKey);
+      }
+
+      analytics.ready(() => {
+        const userID = Cookies.get("ToitUserID");
+        if (userID) {
+          analytics.identify("user/" + userID, {
+            entity_type: "user",
+          });
+        }
+      });
+    }
+  };
+
+  const handleDeclineCookie = () => {
+    if (Cookies.get("toit-cookies")) {
+      Cookies.remove("toit-cookies", { path: "/" });
+    }
+    setUserConsent(false);
+    setShowCookiesConsent(false);
+  };
+
+  const handleDeclineCookieUI = () => {
+    window.sessionStorage.setItem &&
+      window?.sessionStorage?.setItem("disallow-cookies", "true");
+    handleDeclineCookie();
+    window.location.reload();
+  };
+
+  const handleCookies = () => {
+    if (window?.sessionStorage.getItem("disallow-cookies") === "true") {
+      handleDeclineCookie();
+    } else {
+      handleAcceptCookie();
+    }
+  };
+
   useEffect(() => {
-    console.log("key: ", segmentKey);
-  });
+    handleCookies();
+
+    if (Cookies.get("ToitUserID")) {
+      setUserSignedIn(true);
+    }
+    // Check if user has explicitly chosen to opt in or out
+    if (isUserConsent) {
+      handleAcceptCookieUI();
+    } else if (isUserConsent === false) {
+      handleDeclineCookie();
+    }
+    setPageLoaded(true);
+  }, [isUserConsent, show]);
   return (
     <>
-      {showCookieConsent && !manageCookies ? (
+      {((!isUserSignedIn &&
+        showCookieConsent &&
+        !isUserConsent &&
+        isPageLoaded &&
+        manageCookies) ||
+        show) && (
         <Card className={classes.cookieConsentCard}>
-          <div className={classes.cookieConsentTextContent}>
-            <Typography>
-              We use cookies to collect data to improve your user experience. By
-              using our website, you&apos;re agreeing to our{" "}
-              <Link className={classes.link}>cookies policy</Link>. You can
-              change your{" "}
-              <Link
-                onClick={() => setManageCookies(true)}
-                className={classes.link}
-              >
-                preferences
-              </Link>{" "}
-              at any time.
-            </Typography>
-          </div>
-        </Card>
-      ) : (
-        <Card hidden={!manageCookies} className={classes.cookieConsentCard}>
+          <IconButton
+            className={classes.exitButton}
+            onClick={() => handleAcceptCookieUI()}
+          >
+            <FiX />
+          </IconButton>
           <div className={classes.cookieConsentTextContent}>
             <Typography variant="h3">Change your cookie setting</Typography>
             <Typography>
@@ -86,7 +178,7 @@ function Cookie({ segmentKey }: CookieProps): JSX.Element {
               size="medium"
               variant="contained"
               className={classes.button}
-              onClick={() => setShowCookiesConsent(false)}
+              onClick={() => handleDeclineCookieUI()}
             >
               Decline
             </Button>
@@ -94,13 +186,42 @@ function Cookie({ segmentKey }: CookieProps): JSX.Element {
               size="medium"
               variant="contained"
               className={classes.button}
-              onClick={() => setShowCookiesConsent(false)}
+              onClick={() => handleAcceptCookieUI()}
             >
               Accept
             </Button>
           </div>
         </Card>
       )}
+      {!isUserSignedIn &&
+        showCookieConsent &&
+        !isUserConsent &&
+        isPageLoaded &&
+        !manageCookies && (
+          <Card className={classes.cookieConsentCard}>
+            <IconButton
+              className={classes.exitButton}
+              onClick={() => handleAcceptCookieUI()}
+            >
+              <FiX />
+            </IconButton>
+            <div className={classes.cookieConsentTextContent}>
+              <Typography>
+                We use cookies to collect data to improve your user experience.
+                By using our website, you&apos;re agreeing to our . You can
+                change your{" "}
+                <LinkCore
+                  href="javascript:undefined;"
+                  onClick={() => setManageCookies(true)}
+                  className={classes.link}
+                >
+                  preferences
+                </LinkCore>{" "}
+                at any time.
+              </Typography>
+            </div>
+          </Card>
+        )}
     </>
   );
 }
